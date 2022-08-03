b1fe88e98fc466526914636224b3afcfa63e9591
Kuba <kuba.porebski3@gmail.com>
2022-08-03 07:15:41 +0200

    Hafed change suggestion

--- Backend/NoirBank.Data/Entities/Operation.cs
+++ Backend/NoirBank.Data/Entities/Operation.cs
@@ -12,14 +12,13 @@ namespace NoirBank.Data.Entities
 		public string Title { get; set; }
 		public double Amount { get; set; }
 		public TransactionTypes TranscationType { get; set; }
-		public double Balance { get; set; }
-		public double PreviousBalance { get; set; }
+		public DateTimeOffset OperationDate { get; set; }
 		/*[ForeignKey("Recipient")]
 		public Guid? RecipientId { get; set; }
 		public virtual Account Recipient { get; set; }
 		[ForeignKey("Sender")]
-		public Guid? SenderId { get; set; }*/
-		public virtual Account Sender { get; set; }
+		public Guid? SenderId { get; set; }
+		public virtual Account Sender { get; set; } */
 		public OperationTypes OperationType { get; set; }
 
 		public Operation()
