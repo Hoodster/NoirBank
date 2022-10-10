ce6ee99c9cf313d4e3ce87c157b7d3e0a5012ad0
Kuba <kuba.porebski3@gmail.com>
2022-10-10 04:18:39 +0200

    Theme settings modal

---
+++ Frontend/bank-client/src/components/inputs/radio/radio.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './radio.scss'
+
+function Radio(props) {
+	const setRadioChecked = () => {
+		const radioButton = document.getElementById(props.id)
+		radioButton.checked = true
+	}
+
+	return (
+		<div className='nb-radio' onClick={setRadioChecked}>
+			<input id={props.id} name={props.name} type={'radio'} value={props.value} defaultChecked={props.default} />
+			<label htmlFor={props.id}>{props.text}</label>
+		</div>
+	)
+}
+
+export default Radio
--- Frontend/bank-client/src/modals/theme-picker-modal/theme-picker-modal.js
+++ Frontend/bank-client/src/modals/theme-picker-modal/theme-picker-modal.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux'
+import Radio from '../../components/inputs/radio/radio'
 import ModalBase from '../../components/modal/modal-base'
 import { close } from '../../redux/reducers/modal-reducer'
 import { setApplicationTheme } from '../../redux/reducers/settings-reducer'
 
 function ThemePickerModal() {
 	const dispatch = useDispatch()
-	const [theme, setTheme] = useState(null)
+	const currentTheme = localStorage.getItem('theme')
 
 	const primaryAction = {
 		text: 'Change theme',
-		action: () => dispatch(setApplicationTheme(theme))
+		action: () => {
+			const newTheme = document.querySelector('input[name="themeRadioGroup"]:checked').value
+			dispatch(setApplicationTheme(newTheme))
+			dispatch(close())
+		}
 	}
 
 	const secondaryAction = {
@@ -19,12 +24,10 @@ function ThemePickerModal() {
 	}
     
 	return (
-		<ModalBase primaryAction={primaryAction} secondaryAction={secondaryAction} title='Set application theme'>
-			<select onChange={(e) => setTheme(e.target.value)}>
-				<option value={'system'}>Default (system)</option>
-				<option value={'dark'}>Dark</option>
-				<option value={'light'}>Light</option>
-			</select>
+		<ModalBase primaryAction={primaryAction} secondaryAction={secondaryAction} title='Set application theme' contentPosition='center'>
+			<Radio id='system_opt' name='themeRadioGroup' text='Default (system)' value='system' default={currentTheme === 'system' || !currentTheme}/>
+			<Radio id='dark_opt' name='themeRadioGroup' text='Dark' value='dark' default={currentTheme === 'dark'}/>
+			<Radio id='light_opt' name='themeRadioGroup' text='Light' value='light' default={currentTheme === 'light'}/>
 		</ModalBase>
 	)
 }
